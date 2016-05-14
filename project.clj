e5cc7adf88f6a4a8d4a091c256b0f539028ca445
Richard Hull <rm_hull@yahoo.co.uk>
2016-05-14 14:18:19 +0100

    Update project dependencies
    Update to MIT license; make work with latest clojure version

--- project.clj
+++ project.clj
@@ -1,12 +1,32 @@
 (defproject delver/treebank-viz "0.0.1"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lacij "0.8.1"]
-                 [clojure-opennlp "0.3.1"]
-                 [compojure "1.1.5"]
-                 [hiccup "1.0.4"]]
-  :plugins [[lein-ring "0.8.3"]]
-  :ring {:handler treebank-viz.handler/app}
-  :min-lein-version "2.3.2"
-  :global-vars { *warn-on-reflection* true})
+  :description ""
+  :url "https://github.com/rm-hull/treebank-viz"
+  :license {
+    :name "The MIT License (MIT)"
+    :url "http://opensource.org/licenses/MIT"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [lacij "0.9.1"]
+    [instaparse "1.4.2"]
+    [clojure-opennlp "0.3.3"]
+    [compojure "1.5.0"]
+    [hiccup "1.0.5"]]
+  :ring {
+    :handler treebank-viz.handler/app}
+  :plugins [
+    [lein-ring "0.9.7"]]
+  :source-paths ["src"]
+  :resouce-paths ["resouces"]
+  :jar-exclusions [#"(?:^|/).git"]
+  :uberjar-exclusions [#"\.SF" #"\.RSA" #"\.DSA"]
+  :min-lein-version "2.6.1"
+  :global-vars { *warn-on-reflection* true}
+    :profiles {
+    :uberjar {:aot :all}
+    :dev {
+      :global-vars {*warn-on-reflection* true}
+      :dependencies [
+        [org.clojure/test.check "0.9.0"]]
+      :plugins [
+        [lein-cloverage "1.0.6"]]}})
+
